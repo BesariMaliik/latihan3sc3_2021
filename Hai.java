b5de5529063a764e4eb2a941224397fc6ce11464
BesariMaliik <91235468+BesariMaliik@users.noreply.github.com>
2021-12-29 10:34:19 +0700

    Update Hai.java
    commit keempat

--- Hai.java
+++ Hai.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("Judul");
 System.out.println("mjmbm: Aplikasi Java 2021");
 System.out.println("--- *** ---");
+  System.out.println("END");
 }
 }
