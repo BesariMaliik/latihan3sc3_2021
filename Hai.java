9c6b6fd4cfe53389ed09da4f6e6a9ca2a7e7bfdd
sofiav132 <sv132sv132@gmail.com>
2022-01-03 10:33:31 +0700

    add user fork SOUT

--- Hai.java
+++ Hai.java
@@ -1,8 +1,9 @@
 class Hai{
-public static void main(String[] args){
-System.out.println("Judul");
-System.out.println("mjmbm: Aplikasi Java 2021");
-System.out.println("--- *** ---");
-  System.out.println("END");
-}
+  public static void main(String[] args){
+    System.out.println("Judul");
+    System.out.println("mjmbm: Aplikasi Java 2020");
+    System.out.println("--- *** ---");
+    System.out.println("END");
+    System.out.println("Sofia Valentina");
+  }
 }
